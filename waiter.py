29a5b937a95b33b53de6db2fcf6696442b352fd3
John Cobo <john.cobo@gmail.com>
2015-02-08 19:43:18 +0000

    updated for ubuntu

--- waiter.py
+++ waiter.py
@@ -1,6 +1,8 @@
 # This program runs in background and triggers runs of cook.py when a certain file is found.
 # This allows ePubChef users to trigger eBook generation on a remote machine on which files are
 # synced.
+# The code is for running on ubuntu 14.
+
 # The code will check for the existence of a file named waiter.txt in the current directory and
 # all sibling directories. It then reads cook.py arguments from the first line of waiter.py, triggers
 # the job, then comments out the waiter.txt arguments so as not to repeat the run.
@@ -13,12 +15,12 @@ import time
 
 cook_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(cook_dir, os.pardir))
-print("root_dir", root_dir)
+#print("root_dir", root_dir)
 
 def list_dirs():
     # return a list of the current directory (where this scirpt was started and all
     # its sibling directories)
-    dir_names = file_list = next(os.walk('..'))[1]
+    dir_names = file_list = next(os.walk(root_dir))[1]
     full_dirs = []
     for dir in dir_names:
         full_dirs.append(join(root_dir, dir))
@@ -32,11 +34,15 @@ def find_and_run(a_directory):
         print("waiter.txt exists")
         args = read_args(a_directory)
         if args[0][0] != '#':   # ignore if the line is commented out
+            cook_loc = join(a_directory,'cook.py')
+            log_loc = join(a_directory,'cook.log')
+            #print("cook_loc:",cook_loc)
+            #print("log_loc:",log_loc)
             try:
-                output = subprocess.check_output(['python','cook.py',args[0],args[1], '>','cook.log'], shell=True)
+                output = subprocess.check_output(['python3',cook_loc,args[0],args[1], '>',log_loc], shell=False)
             except:
                 # only one argument
-                output = subprocess.check_output(['python','cook.py',args[0], '>','cook.log'], shell=True)
+                output = subprocess.check_output(['python3',cook_loc,args[0], '>',log_loc], shell=False)
             rewrite_waiter(a_directory, args)
         else:
             print("commented out")
